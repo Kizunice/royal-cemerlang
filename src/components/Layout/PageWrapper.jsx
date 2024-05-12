180e698b94d99b219f9e8e7f73fce2b48abc57f1
Kizunice <dev.kizunice@gmail.com>
2024-05-12 16:11:03 +0700

    config motion

--- src/components/Layout/PageWrapper.jsx
+++ src/components/Layout/PageWrapper.jsx
@@ -6,10 +6,10 @@ export const PageWrapper = ({ children }) => (
   <>
     <AnimatePresence>
       <motion.div
-        initial={{ opacity: 0, y: 15 }}
+        initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 15 }}
-        transition={{ delay: 0.25 }}
+        exit={{ opacity: 0, y: 30 }}
+        transition={{ delay: 0.5 }}
       >
         {children}
       </motion.div>
